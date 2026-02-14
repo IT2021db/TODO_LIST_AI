b8b7ee3228594209e3d563fb803a5cd7f9e480ad
Dorota Borowska <youcode2021db@gmail.com>
2026-02-14 17:59:33 +0100

    uninstall flowbite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import flowbiteReact from "flowbite-react/plugin/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), flowbiteReact()]
+  plugins: [react(), tailwindcss()],
 });
