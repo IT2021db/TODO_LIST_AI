c125837b0898d21a693311b584502002aba7af10
Dorota Borowska <youcode2021db@gmail.com>
2026-02-20 15:52:57 +0100

    separation supabase data schema from form schema

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,28 @@
 import { z } from "zod";
 
+//1. DATA SCHEMA FROM SUPABASE
+
+//taskSchema describing structure of a single task in database
+export const taskSchema = z.object({
+  id: z.coerce.number(),
+  text: z.coerce.string(),
+  completed: z.coerce.boolean(),
+});
+
+export const tasksSchema = z.array(taskSchema); //table of tasks
+
+export type Task = z.infer<typeof taskSchema>; //type of TypeScript generated from Zod
+
+//2. FORM SCHEMA - TEXT WRITTEN IN INPUT
+
+//TaskFormSchema - scheme for addNewTask form
+export const taskFormSchema = z.object({
+  task: z.coerce.string().min(1, "Pole nie może być puste"),
+});
+
+export type TaskFormData = z.infer<typeof taskFormSchema>; //type TS for form
+
+//3. TYPE FOR TaskService
 export type TasksService = {
   tasks: Task[];
   loading: boolean;
@@ -9,13 +32,3 @@ export type TasksService = {
   deleteTask: (id: number) => Promise<void>;
   completeAllTasks: () => Promise<void>;
 };
-//type of data from Supabase
-export const taskSchema = z.object({
-  id: z.coerce.number(),
-  text: z.coerce.string(),
-  completed: z.coerce.boolean(),
-});
-
-//type of TypeScript generated from Zod
-export type Task = z.infer<typeof taskSchema>;
-export const tasksSchema = z.array(taskSchema);
