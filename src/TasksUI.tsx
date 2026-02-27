620ac894034950f55b0cc29786ee0fa6e5d61766
Dorota Borowska <youcode2021db@gmail.com>
2026-02-27 12:26:01 +0100

    hide caret outside inputs/buttons

--- src/TasksUI.tsx
+++ src/TasksUI.tsx
@@ -59,7 +59,7 @@ export default function TasksUI({
       <h1 className="bg-teal-500 text-white p-8 w-full h-24">
         <LanguageSwitcher locale={Locale} setLocale={setLocale} />
       </h1>
-      <main className="grid grid-cols-1 mx-auto p-5 max-w-4xl gap-5 max-[767px]:grid-cols-1">
+      <main className="grid grid-cols-1 mx-auto p-5 max-w-4xl gap-5 max-[767px]:grid-cols-1 caret-transparent">
         <h2 className="text-4xl font-bold">
           <FormattedMessage id="title" />
         </h2>
