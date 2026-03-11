1f4ba692fff1cc51c519c524dc3b5bde9067b5f0
Dorota Borowska <youcode2021db@gmail.com>
2026-03-11 15:33:57 +0100

    feat: add global Tooltip component

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import { createRoot } from "react-dom/client";
+import { Tooltip } from "react-tooltip";
 import { IntlProvider } from "react-intl";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import { messages } from "./i18n/messages";
 import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useAppLocale } from "./useAppLocale";
 
 const queryClient = new QueryClient();
@@ -17,6 +18,12 @@ function Root() {
       <IntlProvider locale={locale} messages={messages[locale]}>
         <App locale={locale} onLocaleChange={onLocaleChange} />
       </IntlProvider>
+       {/* GLOBAL TOOLTIP */}
+      <Tooltip
+        id="app-tooltip"
+        place="top"
+        className="bg-slate-700 text-white px-2 py-1 rounded text-sm"
+      />
     </QueryClientProvider>
   );
 }
