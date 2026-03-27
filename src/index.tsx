3e2c7d0d889e244f5a84f10e522ddf8470ea9379
Dorota Borowska <youcode2021db@gmail.com>
2026-03-27 19:05:36 +0100

    refactor: replace intl with i18next

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,19 @@
 import { createRoot } from "react-dom/client";
 import { Tooltip } from "react-tooltip";
-import { IntlProvider } from "react-intl";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
-import { messages } from "./i18n/messages";
 import "./index.css";
-import { useAppLocale } from "./hooks/useAppLocale";
+import './i18n';  //needs to be bundled
 
 const queryClient = new QueryClient();
 function Root() {
-  const { locale, onLocaleChange } = useAppLocale();
 
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
-      <IntlProvider locale={locale} messages={messages[locale]}>
-        <App locale={locale} onLocaleChange={onLocaleChange} />
-      </IntlProvider>
-      {/* GLOBAL TOOLTIP */}
+          <App />
+          {/* GLOBAL TOOLTIP */}
       <Tooltip
         id="app-tooltip"
         place="top"
