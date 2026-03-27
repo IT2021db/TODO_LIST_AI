2091c7e0351385192346035180a2d53648364bb8
Dorota Borowska <youcode2021db@gmail.com>
2026-03-27 19:07:38 +0100

    style: format code (spacing adjustments)

--- src/features/tasks/TasksUILayout.tsx
+++ src/features/tasks/TasksUILayout.tsx
@@ -9,15 +9,12 @@ export default function TasksUILayout({
 }: TasksUILayoutProps) {
   return (
     <div>
-
       <header className="bg-teal-500 text-white p-8 w-full h-24">
         {header}
       </header>
-
       <main className="grid grid-cols-1 mx-auto p-5 max-w-4xl gap-5 caret-transparent">
         {children}
       </main>
-
     </div>
   );
 }
