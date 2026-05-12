bf6e2fccfcbb7d7c722c105ff4b73b7b08eaf0e5
Dorota Borowska <youcode2021db@gmail.com>
2026-05-13 00:18:18 +0200

    refactor: change css style in input

--- src/design-system/Input.tsx
+++ src/design-system/Input.tsx
@@ -7,7 +7,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
     return (
       <input
         ref={ref}
-        className={`bg-[#1A1D24] hover:bg-[#22262f] flex-1 w-full border-3 outline-none rounded-xl border-indigo-500 caret-teal-600  pl-2.5`}
+        className={`bg-[#1A1D24] hover:bg-[#22262f] flex-1 w-full h-14 border-3 outline-none rounded-xl border-indigo-500 caret-teal-600  pl-2.5`}
         {...props}
       />
     );
