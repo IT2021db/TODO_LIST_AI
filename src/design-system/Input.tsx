da798c2c9edcec9ec7ec240bd9d4071c6d64140f
Dorota Borowska <youcode2021db@gmail.com>
2026-05-13 10:33:12 +0200

    refactor(style): set min-with:0 for braking text

--- src/design-system/Input.tsx
+++ src/design-system/Input.tsx
@@ -7,7 +7,22 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
     return (
       <input
         ref={ref}
-        className={`bg-[#1A1D24] hover:bg-[#22262f] flex-1 w-full h-14 border-3 outline-none rounded-xl border-indigo-500 caret-teal-600  pl-2.5`}
+        className={`
+          bg-[#1A1D24]
+          hover:bg-[#22262f]
+          flex-1
+          w-full
+          min-w-0
+          h-14
+          border-3
+          outline-none
+          rounded-xl
+          border-indigo-500
+          caret-teal-600
+          pl-2.5
+          pr-8
+          ${className}
+        `}
         {...props}
       />
     );
